7c878b48fac50eb8e45ffb77304674718b9f5d31
Michael LoCascio <michael.locascio@nrel.gov>
2022-08-11 14:55:04 -0600

    testing branch

--- model.py
+++ model.py
@@ -491,7 +491,6 @@ class ModelComparison:
                 "Major optimality tolerance": 1e-4,
                 "Major feasibility tolerance": 1e-4,
                 "Scale option": 2,
-                "Major iterations limit": 2,
                 },
             timeLimit=timer,
         )
